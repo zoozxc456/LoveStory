d71e3fdc1c6a81a081496a1b3bfca813cd957000
zoozxc456 <zoozxc456@gmail.com>
2024-07-15 11:14:40 +0800

    feat: add create dashboard card component

--- LoveStory.Frontend/pages/index.vue
+++ LoveStory.Frontend/pages/index.vue
@@ -1,7 +1,20 @@
 <template>
-  <h1 class="text-3xl font-bold underline">Hello world!</h1>
+  <div class="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 gap-4 mx-auto">
+    <DashboardCard v-for="card in cards" v-bind="card" />
+  </div>
 </template>
 
-<style scoped lang="scss"></style>
+<script setup lang="ts">
+type Card = {
+  header: string;
+  body: string[];
+};
 
-<script setup lang="ts"></script>
+import { ref } from "vue";
+import DashboardCard from "../components/index/DashboardCard.vue";
+
+const cards = ref<Card[]>([
+  { header: "已邀請", body: ["352 位賓客", "(男方: 100 位 / 女方: 100 位)"] },
+  { header: "已簽到賓客", body: ["72 / 100"] },
+]);
+</script>
