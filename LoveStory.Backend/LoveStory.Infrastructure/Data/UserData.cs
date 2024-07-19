11e4bbcaed7dc368a9b7de2aae86c6f247897604
zoozxc456 <zoozxc456@gmail.com>
2024-07-19 13:41:18 +0800

    feat: add dataset

---
+++ LoveStory.Backend/LoveStory.Infrastructure/Data/UserData.cs
@@ -0,0 +1,22 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace LoveStory.Infrastructure.Data;
+
+[Table("users")]
+public class UserData
+{
+    [Key]
+    [Column("user_id")]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public Guid UserId { get; set; }
+
+    [Column("username")]
+    [StringLength(20)]
+    public string Username { get; set; } = string.Empty;
+
+    public ICollection<BanquetTableData> CreatedTables { get; set; } = new List<BanquetTableData>();
+    public ICollection<GuestData> CreatedGuests { get; set; } = new List<GuestData>();
+    public ICollection<GuestSpecialNeedData> CreatedSpecialNeeds { get; set; } = new List<GuestSpecialNeedData>();
+    public ICollection<GuestGroupData> CreatedGroups { get; set; } = new List<GuestGroupData>();
+}
