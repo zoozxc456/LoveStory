6bee4d2e5682774759808cf3c1d1fd34ae08209e
zoozxc456 <zoozxc456@gmail.com>
2024-08-06 04:31:10 +0800

    fix: fix inject display controller in relationship dropdown list

--- LoveStory.Frontend/components/guests/RelationshipDropDownList.vue
+++ LoveStory.Frontend/components/guests/RelationshipDropDownList.vue
@@ -1,5 +1,5 @@
 <template>
-  <CommonDropDownList>
+  <CommonDropDownList v-model:display-controller="displayController">
     <template #presentation>
       <input
         class="w-full h-full px-3 py-3 font-sans text-sm font-normal transition-all bg-transparent border rounded-md peer border-blue-gray-200 border-t-transparent text-blue-gray-700 outline outline-0 placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 focus:border-2 focus:border-gray-900 focus:border-t-transparent focus:outline-0 disabled:border-0 disabled:bg-blue-gray-50"
