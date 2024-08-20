68dc0907743e4488a8d3594e985d58f81528946d
zoozxc456 <zoozxc456@gmail.com>
2024-08-20 12:57:50 +0800

    feat: add single guest request proxy

--- LoveStory.Frontend/apis/guest.api.ts
+++ LoveStory.Frontend/apis/guest.api.ts
@@ -3,10 +3,6 @@ const generateJwtAuthorizationHeader = () => {
   return ({ Authorization: `Bearer ${getAccessToken()}` });
 };
 
-
-const fetchAllGuests = async () => useAsyncData<GetGuestManagementResponse, ErrorResponse>("http://localhost:5066/api/Guest", () => $fetch<GetGuestManagementResponse>("http://localhost:5066/api/Guest", { headers: { ...generateJwtAuthorizationHeader() } }), { server: true, lazy: false });
-
-const addGuest = async (data: AddGuestManagementRequest) => useAsyncData<any, ErrorResponse>('http://localhost:5066/api/Guest', () => $fetch('http://localhost:5066/api/Guest', { method: 'POST', body: data, headers: { ...generateJwtAuthorizationHeader() } }));
 const deleteGuest = async (data: Pick<IGuest, 'guestId'>) => useAsyncData<any, ErrorResponse>(`http://localhost:5066/api/Guest/${data}`, () => $fetch(`http://localhost:5066/api/Guest/${data}`, { method: 'DELETE', headers: { ...generateJwtAuthorizationHeader() } }));
 
 const modifySingleGuest = async (data: ModifySingleGuestRequest) => useAsyncData<any, ErrorResponse>('http://localhost:5066/api/Guest', () => $fetch('http://localhost:5066/api/Guest', { method: 'PUT', body: data, headers: { ...generateJwtAuthorizationHeader() } }));
