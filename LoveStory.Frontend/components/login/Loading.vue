c4cdad62a73a6dd55e9a257dffe57d275d5596a3
zoozxc456 <zoozxc456@gmail.com>
2024-08-26 19:57:55 +0800

    feat: extract components in login page

---
+++ LoveStory.Frontend/components/login/Loading.vue
@@ -0,0 +1,20 @@
+<template>
+  <div
+    class="h-dvh w-dvw absolute top-0 z-[9999] bg-black/80"
+    v-if="props.isLoading"
+  >
+    <div class="relative w-full h-full">
+      <div class="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+        <span class="text-white text-2xl font-bold"> Loading ... </span>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped lang="scss"></style>
+
+<script setup lang="ts">
+type LoadingProps = { isLoading: boolean };
+
+const props = defineProps<LoadingProps>();
+</script>
