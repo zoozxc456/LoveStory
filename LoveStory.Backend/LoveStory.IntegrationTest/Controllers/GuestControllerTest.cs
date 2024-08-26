77fc5de7eb952f212f0cdb7422754158a7d9779c
zoozxc456 <zoozxc456@gmail.com>
2024-08-27 01:38:27 +0800

    fix: fix guest controller integration test

--- LoveStory.Backend/LoveStory.IntegrationTest/Controllers/GuestControllerTest.cs
+++ LoveStory.Backend/LoveStory.IntegrationTest/Controllers/GuestControllerTest.cs
@@ -1,11 +1,5 @@
-using LoveStory.Core.Interfaces;
-using LoveStory.Core.Services;
-using LoveStory.Infrastructure.Contexts;
-using LoveStory.Infrastructure.Data;
-using LoveStory.Infrastructure.Interfaces;
-using LoveStory.Infrastructure.Repositories;
+using LoveStory.IntegrationTest.Providers;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using GuestController = LoveStory.WebApi.Controllers.GuestController;
 
@@ -21,40 +15,25 @@ public class GuestControllerTest : IDisposable
     [SetUp]
     public void SetUp()
     {
-        SetUpServiceCollection();
         SetUpServiceProvider();
-        SetUpGuestController();       
+        SetUpGuestController();
     }
-    
+
+    private void SetUpServiceProvider()
+    {
+        _provider = new ServiceCollectionProvider().Provider;
+    }
+
     private void SetUpGuestController()
     {
         _guestController = new GuestController(_provider);
     }
 
-    private void SetUpServiceProvider()
-    {
-        _provider = _serviceCollection.BuildServiceProvider();
-    }
-
-    private  void SetUpServiceCollection()
-    {
-        _serviceCollection = [];
-        _serviceCollection.AddControllers();
-        _serviceCollection.AddDbContext<LoveStoryContext>(option=>option.UseInMemoryDatabase("love_story"));
-        _serviceCollection.AddScoped<IGuestService, GuestService>();
-        _serviceCollection.AddScoped<IRepository<UserData>, GenericRepository<UserData>>();
-        _serviceCollection.AddScoped<IRepository<BanquetTableData>, GenericRepository<BanquetTableData>>();
-        _serviceCollection.AddScoped<IRepository<GuestData>, GenericRepository<GuestData>>();
-        _serviceCollection.AddScoped<IRepository<GuestAttendanceData>, GenericRepository<GuestAttendanceData>>();
-        _serviceCollection.AddScoped<IRepository<GuestGroupData>, GenericRepository<GuestGroupData>>();
-        _serviceCollection.AddScoped<IRepository<GuestSpecialNeedData>, GenericRepository<GuestSpecialNeedData>>();
-    }
-
     [Test]
-    public void GetAllGuests_GivenEmptyParameter_ShouldReturnOkResult()
+    public void GetAllGuests_GivenEmptyParameter_ShouldReturnOkObjectResult()
     {
         var actionResult = _guestController.GetAllGuests();
-        Assert.That(actionResult, Is.InstanceOf<OkResult>());
+        Assert.That(actionResult, Is.InstanceOf<OkObjectResult>());
     }
 
     public void Dispose()
