e11715bcd3e1667e3bf17fef9f9474c17f2acbb5
zoozxc456 <zoozxc456@gmail.com>
2024-08-27 10:46:41 +0800

    feat: add role and is_needed_reset_password columns for user

--- LoveStory.Backend/LoveStory.Infrastructure/Migrations/20240827023708_ADD_COLUMN_FOR_USER.Designer.cs
+++ LoveStory.Backend/LoveStory.Infrastructure/Migrations/20240827023708_ADD_COLUMN_FOR_USER.Designer.cs
@@ -4,6 +4,7 @@ using LoveStory.Infrastructure.Contexts;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace LoveStory.Infrastructure.Migrations
 {
     [DbContext(typeof(LoveStoryContext))]
-    partial class LoveStoryContextModelSnapshot : ModelSnapshot
+    [Migration("20240827023708_ADD_COLUMN_FOR_USER")]
+    partial class ADD_COLUMN_FOR_USER
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
