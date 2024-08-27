3a60e32ecf3117a0641b96f6a678dad8d0bb1329
zoozxc456 <zoozxc456@gmail.com>
2024-08-28 01:33:11 +0800

    feat: add create_at and creator for user data

---
+++ LoveStory.Backend/LoveStory.Infrastructure/Migrations/20240827172747_ADD_COLUMNS_FOR_USER.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LoveStory.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class ADD_COLUMNS_FOR_USER : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "create_at",
+                table: "users",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "creator",
+                table: "users",
+                type: "uniqueidentifier",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_users_creator",
+                table: "users",
+                column: "creator");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_users_users_creator",
+                table: "users",
+                column: "creator",
+                principalTable: "users",
+                principalColumn: "user_id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_users_users_creator",
+                table: "users");
+
+            migrationBuilder.DropIndex(
+                name: "IX_users_creator",
+                table: "users");
+
+            migrationBuilder.DropColumn(
+                name: "create_at",
+                table: "users");
+
+            migrationBuilder.DropColumn(
+                name: "creator",
+                table: "users");
+        }
+    }
+}
