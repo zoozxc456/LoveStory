43a1fbca56a67eda1805b8e2725888cdef848a89
zoozxc456 <zoozxc456@gmail.com>
2025-01-01 23:24:05 +0800

    feat: no password authenticate in dev

--- LoveStory.Backend/LoveStory.Core/Interfaces/ILoginService.cs
+++ LoveStory.Backend/LoveStory.Core/Interfaces/ILoginService.cs
@@ -5,4 +5,5 @@ namespace LoveStory.Core.Interfaces;
 public interface ILoginService
 {
     public Task<(bool, string?)> Login(LoginRequestDto requestDto);
+    public Task<(bool, string?)> Login(DevelopLoginRequestDto requestDto);
 }
